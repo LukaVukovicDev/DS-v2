91805970e232d2da1f2a956e7654b0978239cbda
Luka Vukovic <luka.vukovic288@gmail.com>
2026-03-05 16:00:11 +0100

    Add login page

---
+++ GUI/GUI/LoginWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using GUI;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GUI
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Login_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameBox.Text;
+            string password = PasswordBox.Password;
+
+            // privremena provera (kasnije ide baza)
+            if (username == "admin" && password == "admin")
+            {
+                MainWindow main = new MainWindow();
+                main.Show();
+
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password");
+            }
+        }
+    }
+}
